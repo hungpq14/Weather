3dedf476c0c87bbdd95033ed1ed52a29c6f2b62d
Pham Minh Hoang Linh <hlinh0411hd@gmail.com>
2016-11-30 14:07:32 +0700

    Add About us

--- WeatherApp/app/src/main/java/dtui/fit/com/weatherapp/Activity/AboutActivity.java
+++ WeatherApp/app/src/main/java/dtui/fit/com/weatherapp/Activity/AboutActivity.java
@@ -1,16 +1,46 @@
 package dtui.fit.com.weatherapp.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import dtui.fit.com.weatherapp.Base.BaseFontActivity;
+import dtui.fit.com.weatherapp.Base.BaseToolbarActivity;
 import dtui.fit.com.weatherapp.R;
 
-public class AboutActivity extends BaseFontActivity {
+public class AboutActivity extends BaseToolbarActivity {
+
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_about;
+    }
+
+    @Override
+    public String getToolbarText() {
+        return "About Us";
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+
+        initDraw();
+    }
+
+    private void initDraw() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        int statusBarHeight = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+        }
+        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) toolbar.getLayoutParams();
+        lp.setMargins(0, statusBarHeight, 0, 0);
+        toolbar.setLayoutParams(lp);
+
     }
 }
